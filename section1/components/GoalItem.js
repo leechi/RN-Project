9504ecbe78f0be4b92b123f2baf9a7666883653d
leechi <leechi2228@gmail.com>
2023-12-20 17:03:14 +0900

    style: Add if press flow animation

--- section1/components/GoalItem.js
+++ section1/components/GoalItem.js
@@ -2,11 +2,15 @@ import { StyleSheet, View, Text, Pressable } from "react-native";
 
 const GoalItem = (props) => {
   return (
-    <Pressable onPress={props.onDeleteItem.bind(this, props.id)}>
-      <View style={styles.goalItem}>
+    <View style={styles.goalItem}>
+      <Pressable
+        android_ripple={{ color: "#210644" }}
+        onPress={props.onDeleteItem.bind(this, props.id)}
+        style={({ pressed }) => pressed && styles.pressedItem}
+      >
         <Text style={styles.goalText}>{props.text}</Text>
-      </View>
-    </Pressable>
+      </Pressable>
+    </View>
   );
 };
 
@@ -17,9 +21,12 @@ const styles = StyleSheet.create({
     backgroundColor: "#5e0acc",
     borderRadius: 6,
     margin: 8,
-    padding: 8,
+  },
+  pressedItem: {
+    opacity: 0.5,
   },
   goalText: {
+    padding: 8,
     color: "white",
   },
 });
