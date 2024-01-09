4fdf2dae624f583f4094a5d468ff8075a30b3242
leechi <leechi2228@gmail.com>
2024-01-09 13:19:50 +0900

    feat: Add color system

--- kakao-bus/App.js
+++ kakao-bus/App.js
@@ -1,20 +1,60 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  FlatList,
+  SafeAreaView,
+  SectionList,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={{ flex: 1 }}>
+        <Text style={{ fontSize: 30, fontWeight: "bold" }}>FlatList</Text>
+        <FlatList
+          data={[
+            { busNum: 146 },
+            { busNum: 360 },
+            { busNum: 740 },
+            { busNum: 3412 },
+            { busNum: 1100 },
+            { busNum: 1700 },
+          ]}
+          renderItem={({ item }) => <Text>{item.busNum}</Text>}
+        />
+      </View>
+
+      <View style={{ flex: 1 }}>
+        <Text style={{ fontSize: 30, fontWeight: "bold" }}>SectionList</Text>
+        <SectionList
+          sections={[
+            {
+              title: "간선버스",
+              data: [{ busNum: 146 }, { busNum: 360 }, { busNum: 740 }],
+            },
+            {
+              title: "지선버스",
+              data: [{ busNum: 3412 }],
+            },
+            {
+              title: "직행버스",
+              data: [{ busNum: 1100 }, { busNum: 1700 }],
+            },
+          ]}
+          renderSectionHeader={({ section: { title } }) => <Text>{title}</Text>}
+          renderItem={({ item }) => <Text>{item.busNum}</Text>}
+        />
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
